821f0c2c524c5a1115b41ccb30470ec3df46238c
SeoyoonPark <tjdbs118@gmail.com>
2021-09-26 23:45:04 +0900

    3주차_1

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
+
+
+
 var db;
 MongoClient.connect('mongodb+srv://tjdbs118:zhzhdk118@cluster0.b56ue.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
  function(에러, client){
@@ -34,17 +38,21 @@ app.get('/write', function(요청, 응답){
     응답.sendFile(__dirname + '/write.html')
 });
 
-//어떤 사람이 /add 경로로 POST 요청을 하면...
-//??를 해주세요~
-
 app.post('/add', function(요청, 응답){
     응답.send('전송완료');
     db.collection('post').insertOne({제목: 요청.body.title, 날짜: 요청.body.date}, function(){
         console.log('저장완료');
     });
+
 });
 
-//어떤 사람이 /add라는 경로로 post 요청을 하면,
-//데이터 2개(날짜, 제목)을 보내주는데,
-//이 때 'post'라는 이름을 가진 collection에 2개 데이터 저장하기
-//{제목: '어쩌구, 날짜: '어쩌구'}
+app.get('/list', function(요청, 응답){
+
+    db.collection('post').find().toArray(function(에러, 결과){
+        console.log(결과);
+        응답.render('list.ejs', {posts : 결과 });
+    }); //모든데이터 가져오기
+
+    
+
+});
