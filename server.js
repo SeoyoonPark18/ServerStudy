f270712c66247a151dbf6702ea67e121c6bb34b2
SeoyoonPark <tjdbs118@gmail.com>
2021-10-04 20:49:38 +0900

    5주차

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 
 const MongoClient = require('mongodb').MongoClient;
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
 app.set('view engine', 'ejs');
 
 
@@ -79,4 +82,20 @@ app.get('/detail/:id', function(요청,응답){
         응답.render('detail.ejs',{data : 결과 })
     })
     
+})
+
+app.get('/edit/:id', function(요청, 응답){
+    db.collection('post').findOne({_id: parseInt(요청.params.id)}, function(에러, 결과){
+        응답.render('edit.ejs', {post : 결과 })
+        console.log(결과);
+    })
+    
+})
+
+app.put('/edit', function(요청, 응답){
+    db.collection('post').updateOne({_id: parseInt(요청.body.id)},{$set: {제목: 요청.body.title, 날짜: 요청.body.date}}, 
+    function(에러, 결과){
+        console.log('수정완료');
+        응답.redirect('/list')
+    })
 })
